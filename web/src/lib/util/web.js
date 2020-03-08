27dbc112057af37865fddb25d0f72283bb44b480
anton <anton>
2020-03-08 09:17:06 +0000

    Web changes

--- web/src/lib/util/web.js
+++ web/src/lib/util/web.js
@@ -1,26 +1,44 @@
 
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+// If separator is a regular expression that contains capturing parentheses (),
+// matched results are included in the array.
+const formatRegex = /\{(?:([0-9]*(?:\.[0-9]+)?)x)?(?:(\.[0-9]*)?f)?\}/g;
 export function formatNumber(num, fmt) {
   if(fmt === "" || fmt == undefined) {
     return String(num);
   }
 
-  let rgx      = /\{(?:([0-9]*(?:\.[0-9]+)?)x)?(?:(?:\.([0-9]*))?f)?\}/g;
-  let matches  = [...fmt.matchAll(rgx)];
-  let literals = fmt.split(rgx);
+  let splits   = fmt.split(formatRegex);
+  let matches  = [];
+  splits.filter((d, i) => i % 3 != 0).map(
+    (d, i, arr) => i % 2 == 0 ? matches.push([d, arr[i+1]]) : false
+  );
+  let literals = splits.filter((d, i) => i % 3 == 0).map(d => d.replace(/\\([{}])/, "$1"));
   let result   = "";
 
   for(let i = 0; i < matches.length; i++) {
-    let m = matches[i];
-    let [coef, prcs] = [m[1], m[2]];
+    let [coef, prcs] = matches[i];
     coef = coef ? Number(coef) : 1;
-    prcs = prcs ? Number(prcs) : 0;
+    
+    // Precision
+    let modified = num * coef;
+    if(prcs) {
+      prcs = prcs.substring(1);
+      prcs = prcs === "" ? 0 : Number(prcs);
+      modified = modified.toFixed(prcs);
+    }
 
-    let modified = (num * coef).toFixed(prcs);
-    result += literals[i].replace(/\\([{}])/, "$1");
-    result += modified;
+    result += literals[i];
+    result += formatComma(modified);
   }
   result += literals[literals.length - 1];
 
   return result;
+}
+
+function formatComma(x) {
+  var parts = x.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return parts.join(".");
 }
--- web/src/main.js
+++ web/src/main.js
@@ -28,28 +28,6 @@ Number.prototype.date = function(str) {
 Number.prototype.format = function(fmt) {
   return formatNumber(this, fmt);
 }
-let n = 1.503;
-console.log(n.format("{}"));
-console.log(n.format("{.4f}"));
-console.log(n.format("{.f}"));
-console.log(n.format("{f}%"));
-console.log(n.format("\\{{f}\\}"));
-console.log(n.format("{2.0x}"));
-console.log(n.format("{3x.f}"));
-
-
-function _temp1(str) {
-  if(str === "" || str === undefined) str = "{}";
-  var fmt = parseNumberFormat(str);
-  var num = this;
-  num = num * Math.pow(10, fmt.exp);
-  // Precision
-  if(!isNaN(fmt.precision)) {
-    var precision = Math.pow(10, fmt.precision);
-    num = Math.round(num * precision) / precision;
-  }
-  return `${fmt.prefix}${formatComma(num)}${fmt.suffix}`;
-};
 
 Number.prototype.toSeries = function() {
   return "series-" + "abcdefghijklmnopqrstuvwxyz".charAt(this);
@@ -67,20 +45,7 @@ function getSeriesIdx(i) {
   return "abcdefghijklmnopqrstuvwxyz".charAt(i - 1);
 }
 
-function parseNumberFormat(str) {
-  var rgx = /(.+)?(\{(e([+-]?\d+))?(\.(\d+))?f?\})(.+)?/;
-  var m = str.match(rgx);
-  var [prefix = "", exp = 0, precision = 2, suffix = ""] = [m[1], m[4], m[6], m[7]];
-  return {
-    prefix, exp: parseInt(exp), precision: parseInt(precision), suffix
-  };
-}
 
-function formatComma(x) {
-  var parts = x.toString().split(".");
-  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  return parts.join(".");
-}
 
 // MAIN
 (async function() {
